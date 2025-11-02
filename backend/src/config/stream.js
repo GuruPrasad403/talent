6fbfeae7e47196abfda15fd4a67ef50afe11af38
guruprasad403 <guruprasas27@gmail.com>
2025-11-02 20:35:39 +0530

    auth-stream-synce

---
+++ backend/src/config/stream.js
@@ -0,0 +1,29 @@
+import {StreamChat} from "stream-chat"
+import { ENV } from "./env.js"
+
+const apiKey = ENV.STREAM_API_KEY;
+const apiSecret = ENV.STREAM_API_SECRET;
+
+if(!apiKey || !apiSecret) {
+    console.error("STREAM_API_KEY or STREAM_API_SECRET is missing");
+}
+
+const chatClient = StreamChat.getInstance(apiKey,apiSecret)
+
+
+export const updateUser= async (userData)=>{
+    try {
+        await chatClient.upsertUser(userData);
+        return userData
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+export const deleteUser = async (userId)=>{
+    try {
+        await chatClient.deleteUser(userId)
+    } catch (error) {
+        console.error(error)
+    }
+}
