b0839f40f3860ae05557e41041f1bcbe57a971d7
guruprasad403 <guruprasas27@gmail.com>
2025-11-02 19:27:21 +0530

    env-update

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -5,8 +5,8 @@ export const ENV = {
   PORT: process.env.PORT,
   DB: process.env.DB,
   NODE_ENV: process.env.NODE_ENV,
-  INNGST_EVENT_KEY: process.env.INNGST_EVENT_KEY,
-  INNGST_SIGNING_KEY: process.env.INNGST_SIGNING_KEY,
+  INNGEST_EVENT_KEY: process.env.INNGST_EVENT_KEY,
+  INNGEST_SIGNING_KEY: process.env.INNGEST_SIGNING_KEY,
   STREAM_API_KEY: process.env.STREAM_API_KEY,
   STREAM_API_SECRET: process.env.STREAM_API_SECRET,
   CLIENT_URL : process.env.CLIENT_URL
