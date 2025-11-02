e0986dfa93da6e389db1db2aae1f08a37d61eb6a
guruprasad403 <guruprasas27@gmail.com>
2025-11-02 19:17:54 +0530

    spelling issue

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -5,8 +5,8 @@ export const ENV = {
   PORT: process.env.PORT,
   DB: process.env.DB,
   NODE_ENV: process.env.NODE_ENV,
-  INNEGST_EVENT_KEY: process.env.INNEGST_EVENT_KEY,
-  INNEGST_SIGNING_KEY: process.env.INNEGST_SIGNING_KEY,
+  INNGST_EVENT_KEY: process.env.INNGST_EVENT_KEY,
+  INNGST_SIGNING_KEY: process.env.INNGST_SIGNING_KEY,
   STREAM_API_KEY: process.env.STREAM_API_KEY,
   STREAM_API_SECRET: process.env.STREAM_API_SECRET,
   CLIENT_URL : process.env.CLIENT_URL
