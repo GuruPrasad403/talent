5ac2af351048bcb58e6bfcfb96cc308a073ac607
guruprasad403 <guruprasas27@gmail.com>
2025-11-02 18:44:26 +0530

    auth-db-sync

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -1,10 +1,13 @@
-import dotenv from 'dotenv'
-dotenv.config({quiet : true});
-
+import dotenv from 'dotenv';
+dotenv.config ({quiet: true});
 
 export const ENV = {
-    PORT : process.env.PORT,
-    DB : process.env.DB,
-    NODE_ENV: process.env.NODE_ENV,
-
-} 
+  PORT: process.env.PORT,
+  DB: process.env.DB,
+  NODE_ENV: process.env.NODE_ENV,
+  INNEGST_EVENT_KEY: process.env.INNEGST_EVENT_KEY,
+  INNEGST_SIGNING_KEY: process.env.INNEGST_SIGNING_KEY,
+  STREAM_API_KEY: process.env.STREAM_API_KEY,
+  STREAM_API_SECRET: process.env.STREAM_API_SECRET,
+  CLIENT_URL : process.env.CLIENT_URL
+};
