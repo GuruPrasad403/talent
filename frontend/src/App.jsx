82b1f13c175ef137cdd78e86c48795fc495ffc15
guruprasad403 <guruprasas27@gmail.com>
2025-11-03 20:07:09 +0530

    sign issue

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,22 +4,17 @@ import { useUser } from '@clerk/clerk-react'
 import HomePage from './pages/HomePage'
 import ProblemPage from './pages/ProblemPage'
 import { Toaster } from 'react-hot-toast'
-import Dashbord from './pages/Dashbord.jsx'
-/**
- * Root React component that renders the app header and Clerk authentication UI.
- *
- * Renders a welcome heading, a SignOutButton when a user is signed in, a SignInButton (modal) when signed out, and a persistent UserButton.
- * @returns {JSX.Element} The application's top-level UI fragment.
- */
+import Dashboard from './pages/Dashbord'
+
 function App() {
-// clrek give useUser hook to check weather the user is signed or not. 
-const {isSigned} = useUser()
+  // clrek give useUser hook to check weather the user is signed or not. 
+  const { isSigned } = useUser()
   return (
     <>
       <Routes>
-        <Route path ="/" element={isSigned ? <HomePage></HomePage> : <Navigate to={"/dashboard"} />}  ></Route>
-        <Route path ="/dashbord" element={isSigned ? <Dashbord></Dashbord> : <Navigate to={"/"}></Navigate>}></Route>
-        <Route path ="/problem-page" element={isSigned ? <ProblemPage></ProblemPage> : <Navigate to={"/"}></Navigate>}></Route>
+        <Route path="/" element={isSigned ? <HomePage /> : <Dashboard /> }  ></Route>
+        <Route path="/dashboard" element={isSigned? <Dashboard />  : <Navigate to={"/"} />}></Route>
+        <Route path="/problem-page" element={isSigned ? <ProblemPage></ProblemPage> : <Navigate to={"/"}></Navigate>}></Route>
       </Routes>
       <Toaster />
     </>
--- frontend/src/pages/Dashbord.jsx
+++ frontend/src/pages/Dashbord.jsx
@@ -1,8 +1,17 @@
+import { SignedIn, SignedOut, SignInButton, SignOutButton, UserButton } from '@clerk/clerk-react'
 import React from 'react'
 
 function Dashbord() {
   return (
-    <div>Dashbord</div>
+    <div>Dashbord
+        <SignedIn>
+            <SignOutButton></SignOutButton>
+        </SignedIn>
+        <SignedOut>
+            <SignInButton></SignInButton>
+        </SignedOut>
+    <UserButton />
+    </div>
   )
 }
 
