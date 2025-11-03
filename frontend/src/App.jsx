abed617d6dfee46a51ea50a4e75821c0abfbc963
guruprasad403 <guruprasas27@gmail.com>
2025-11-03 20:18:18 +0530

    sign issue

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,14 +7,24 @@ import { Toaster } from 'react-hot-toast'
 import Dashboard from './pages/Dashbord'
 
 function App() {
-  // clrek give useUser hook to check weather the user is signed or not. 
-  const { isSigned } = useUser()
+  // Clerk provides the useUser hook to check if the user is signed in
+  const { isSignedIn } = useUser()
+
   return (
     <>
       <Routes>
-        <Route path="/" element={isSigned ? <HomePage /> : <Dashboard /> }  ></Route>
-        <Route path="/dashboard" element={isSigned? <Dashboard />  : <Navigate to={"/"} />}></Route>
-        <Route path="/problem-page" element={isSigned ? <ProblemPage></ProblemPage> : <Navigate to={"/"}></Navigate>}></Route>
+        <Route
+          path="/"
+          element={isSignedIn ? <Navigate to="/dashboard" /> : <HomePage />}
+        />
+        <Route
+          path="/dashboard"
+          element={isSignedIn ? <Dashboard /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/problem-page"
+          element={isSignedIn ? <ProblemPage /> : <Navigate to="/" />}
+        />
       </Routes>
       <Toaster />
     </>
